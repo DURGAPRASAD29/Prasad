8dbc13dcea8477305ce618bc902267d427178cd1
DURGAPRASAD29 <61829148+DURGAPRASAD29@users.noreply.github.com>
2020-08-22 12:50:18 +0530

    Updated 21

---
+++ Prasad/src/com/test/Prasadif.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class Prasadif {
+	public static void main(String[] arg) {
+
+		int v = 18;
+
+		if (v == 18) {
+			System.out.println("Sucessfully Regestered");
+		} else {
+			System.out.println("Regetected");
+		}
+	}
+}
